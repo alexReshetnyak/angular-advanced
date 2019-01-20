8d49ff235dfa7966058962fee7bc9d93ab1fda40
A Reshetnyak <alexreshetnyakmail@gmail.com>
2019-01-20 21:44:10 +0200

    added sw app

---
+++ sw/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// ! ng add @angular/pwa
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sw';
+}
