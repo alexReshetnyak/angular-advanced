992634047101602a14369514fe6312006280f98b
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2019-01-31 17:44:45 +0200

    was created securoty application

--- ssr/src/app/app.component.ts
+++ ssr/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TransferState, makeStateKey } from '@angular/platform-browser';
 
+// * ng generate universal ngw-app-shell --client-project ssr
+
 const ITEMS = makeStateKey('items');
 
 @Component({
@@ -14,8 +16,11 @@ export class AppComponent {
   constructor(
     state: TransferState // * service that working like storage
   ) {
-    state.set(ITEMS, [11]);
     const items = state.get(ITEMS, []);
+
+    if (!items) {
+      state.get(ITEMS, []);
+    }
 
     console.log('appComp Items:', items);
   }
